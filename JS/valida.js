f6adf537fb50ffdce57e6558ccdc3fec6a2098d4
ARZATE-BELLO-SERGIO-SACHIEL <sarzateb1900@alumno.ipn.mx>
2021-03-22 19:58:56 -0600

    return validar

---
+++ JS/valida.js
@@ -0,0 +1,9 @@
+function validar(formulario){
+    /*queremos validadr que se escriban mas de 5 caracteres
+    em el campo de nombre*/
+    if(formulario.nombre.value.length < 5){
+        alert("por favor escribe mas de 5 caracteres en el campo nomber");
+        formulario.nombre.focus();
+    return false;
+    }
+}
