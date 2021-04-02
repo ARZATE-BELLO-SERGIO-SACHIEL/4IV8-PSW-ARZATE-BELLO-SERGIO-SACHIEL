021116af27131f3119a88b75d2fecba58e5e5068
ARZATE-BELLO-SERGIO-SACHIEL <sarzateb1900@alumno.ipn.mx>
2021-04-02 13:39:02 -0600

    se agregaron el problema 3 y 4

--- JS/problemas.js
+++ JS/problemas.js
@@ -56,6 +56,102 @@ function borrar2(){
     document.formulario2.salariot.value = "";
 }
 
+function validard(d){
+    var tecalado = (document.all)?d.KeyCode:d.which;
+    if(tecalado == 8)return true;
+
+    var patron = /[0-9\d .]/;
+    
+    var prueva = String.fromCharCode(tecalado);
+    return patron.test(prueva);
+}
+
+function CalcularDescuento(){
+    var valor = document.formulario3.compra.value;
+    var result = parseInt(valor);
+    var descuento = result*0.15
+    var descuentot = descuento;
+    var descuentoa = valor-descuentot;
+
+    document.formulario3.descuento.value = "$"+descuentot;
+    document.formulario3.pagar.value = "$"+descuentoa;
+
+}
+
+function borrar3(){
+    document.formulario3.compra.value = "";
+    document.formulario3.descuento.value = "";
+    document.formulario3.pagar.value = "";
+}
+
+function ValidarCal(d){
+   
+    var tecalado = (document.all)?d.KeyCode:d.which;
+    if(tecalado == 8)return true;
+
+    var patron = /[0-9\d .]/;
+    
+    var prueva = String.fromCharCode(tecalado);
+    return patron.test(prueva);    
+}
+
+function CalcularCal(){
+    var valor = document.formulario4.calificaciones.value;
+    var valorp = parseFloat(valor);
+
+    if(valorp > 100){
+        alert("Escribe solo numeros entre 0 al 100");
+        document.formulario4.calificaciones.focus();
+        return false;
+    }
+
+    var calfp = valorp*0.55
+
+    var valorexamen = document.formulario4.examen.value;
+    var valorexamenp = parseFloat(valorexamen);
+
+    if(valorexamenp > 100){
+        alert("Escribe solo numeros entre 0 al 100");
+        document.formulario4.examenp.focus();
+        return false;
+    }
+
+    var exmp = valorexamenp*0.30
+
+    var valortrabajo = document.formulario4.trabajo.value;
+    var valortrabajop = parseFloat(valortrabajo);
+
+    if(valortrabajop > 100){
+        alert("Escribe solo numeros entre 0 al 100");
+        document.formulario4.trabajo.focus();
+        return false;
+    }
+
+    var trabp = valortrabajop*0.15;
+
+    var total = valorp+valorexamenp+valortrabajop;
+    var totalp = (calfp+exmp+trabp)/10;
+
+    document.formulario4.calificacionesp.value = calfp.toFixed(2);
+    document.formulario4.examenp.value = exmp.toFixed(2);
+    document.formulario4.trabajop.value = trabp.toFixed(2);
+    document.formulario4.total.value = total.toFixed(2);
+    document.formulario4.totalp.value = totalp.toFixed(2);
+}
+
+function borrar4(){
+    document.formulario4.calificaciones.value = "";
+    document.formulario4.examenp.value = "";
+    document.formulario4.trabajo.value = "";
+    document.formulario4.calificacionesp.value = "";
+    document.formulario4.examenp.value = "";
+    document.formulario4.trabajop.value = "";
+    document.formulario4.total.value = "";
+    document.formulario4.totalp.value = "";
+}
+
+
+
 
  
 
