0db0f623dd30fd37db7727e7664046ad3bfe00d7
ARZATE-BELLO-SERGIO-SACHIEL <sarzateb1900@alumno.ipn.mx>
2021-04-02 00:29:19 -0600

    problema 1

--- JS/problemas.js
+++ JS/problemas.js
@@ -1,1 +1,27 @@
-function
+function validar(a){
+    var teclado = (document.all)?a.KeyCode:a.which;
+    if(teclado == 8)return true; 
+
+    var patron = /[0-9\d .]/;
+
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+
+function interes(){
+    var valor = document.formulario.cantidad.value;
+    var valor2 = document.formulario.mes.value;
+    var result = parseInt(valor);
+    var result2 = parseInt(valor2);
+    var interes = result*0.02*result2;
+    var total = result+interes;
+
+    document.formulario.sueldot.value = "$"+total;
+}
+
+function borrar(){
+    document.formulario.cantidad.value = "";
+    document.formulario.mes.value = "";
+    document.formulario.sueldot.value = "";
+}
+
